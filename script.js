6ceddf67060ee9869dfedb9ed7cfa9b5afb470ed
Nathan <pythoniszen@gmail.com>
2023-07-28 20:48:59 -0700

    7/28/23 Add functionality for all operations

--- script.js
+++ script.js
@@ -1,16 +1,67 @@
 let display = document.getElementById('display');
+let lastOperation = null;
+let lastNumber = null;
 
 function append(num) {
-  display.value += num;
+  if (typeof num === 'number' || !isNaN(num)) {
+    display.value += num;
+  } else {
+    if (['+', '-', '*', '/'].includes(display.value[display.value.length - 1])) {
+      display.value = display.value.slice(0, -1) + num;
+      lastOperation = num;
+    } else {
+      if (lastOperation) {
+        calculate();
+      }
+      lastOperation = num;
+      lastNumber = parseFloat(display.value);
+      display.value += num;
+    }
+  }
 }
 
 function clearDisplay() {
   display.value = '';
+  lastOperation = null;
+  lastNumber = null;
 }
 
 function calculate() {
-  try {
-  } catch {
-    display.value = 'Error';
+  if (lastNumber !== null && lastOperation) {
+    if (['+', '-', '*', '/'].includes(display.value[display.value.length - 1])) {
+      return;
+    }
+    let currentNumber = parseFloat(display.value.split(lastOperation)[1]);
+    if (isNaN(currentNumber)) {
+      return;
+    }
+    let result;
+
+    switch (lastOperation) {
+      case '+':
+        result = lastNumber + currentNumber;
+        break;
+      case '-':
+        result = lastNumber - currentNumber;
+        break;
+      case '*':
+        result = lastNumber * currentNumber;
+        break;
+      case '/':
+        if (currentNumber !== 0) {
+          result = lastNumber / currentNumber;
+        } else {
+          display.value = 'Error';
+          return;
+        }
+        break;
+      default:
+        display.value = 'Error';
+        return;
+    }
+
+    display.value = result;
+    lastNumber = result;
   }
 }
+
